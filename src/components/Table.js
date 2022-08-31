68ff75ecfdfd09460b1bf30d42259d2ed25765be
Windson Macedo <windsondev@hotmail.com>
2022-08-31 13:20:41 -0300

    requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,6 +5,19 @@ class Table extends Component {
     return (
       <div>
         Table
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
       </div>
     );
   }
